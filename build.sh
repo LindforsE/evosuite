736f0f7afa1b513efa46e24bff594650496cdfcb
LindforsE <45037228+LindforsE@users.noreply.github.com>
2022-06-04 10:31:51 +0200

    Updated locations in build script
    Changed location from "bin/evosuite.jar" to "bin/SF100/lib/evosuite.jar"

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ mvn clean
 # build with maven, skip tests
 mvn -e -B package -DskipTests=true
 
-sudo rm bin/evosuite-tests.jar bin/evosuite.jar
+sudo rm bin/SF100/lib/evosuite-tests.jar bin/SF100/lib/evosuite.jar
 
 # move master/target/evosuite-master-*.jar to newly created bin folder
-sudo mv master/target/evosuite-master-*-tests.jar bin/evosuite-tests.jar
-sudo mv master/target/evosuite-master-*.jar bin/evosuite.jar
+sudo mv master/target/evosuite-master-*-tests.jar bin/SF100/lib/evosuite-tests.jar
+sudo mv master/target/evosuite-master-*.jar bin/SF100/lib/evosuite.jar
