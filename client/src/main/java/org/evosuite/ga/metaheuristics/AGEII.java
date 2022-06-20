131fe3feeed8912ba42993c5499610fe3cf45229
LindforsE <45037228+LindforsE@users.noreply.github.com>
2022-06-20 16:13:49 +0200

    AGEII updates + tests
    Updated the implementation of AGEII, and implemented some tests for the floor-method.

--- client/src/main/java/org/evosuite/ga/metaheuristics/AGEII.java
+++ client/src/main/java/org/evosuite/ga/metaheuristics/AGEII.java
@@ -1,9 +1,8 @@
 package org.evosuite.ga.metaheuristics;
 
+// these can be replaced with "import java.util.*;"
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
-
-// these can be replaced with "import java.util.*;"
 import java.util.List;
 
 import org.evosuite.Properties;
@@ -11,7 +10,9 @@ import org.evosuite.ga.Chromosome;
 import org.evosuite.ga.ChromosomeFactory;
 import org.evosuite.ga.FitnessFunction;
 import org.evosuite.ga.comparators.RankAndCrowdingDistanceComparator;
+import org.evosuite.ga.comparators.DominanceComparator;
 import org.evosuite.utils.Randomness;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,10 +30,12 @@ public class AGEII<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
     // Properties.EPSILON (default value should be 0.1)
 
     // epsilon (approximate) archive
+    List<T> approxArchive = null;
 
     // Constructor
     public AGEII(ChromosomeFactory<T> factory) {
         super(factory);
+        approxArchive = new ArrayList<>();
     }
 
     @Override
@@ -49,7 +52,7 @@ public class AGEII<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
 
         // for j <- 1 to lambda, do
         // WHAT IS LAMBDA? (mu is Population size, lambda is ???)
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < (this.population.size() / 2); i++) {
             // Select two individuals from Population
             T parent1 = this.selectionFunction.select(population);
             T parent2 = this.selectionFunction.select(population);
@@ -81,33 +84,38 @@ public class AGEII<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
             // Add new individuals to offspring population
             offspringPopulation.add(offspring1);
             offspringPopulation.add(offspring2);
-
-            this.rankingFunction.computeRankingAssignment(offspringPopulation,
-                    new LinkedHashSet<FitnessFunction<T>>(this.getFitnessFunctions()));
         }
+
+        //this.rankingFunction.computeRankingAssignment(offspringPopulation,
+        //            new LinkedHashSet<FitnessFunction<T>>(this.getFitnessFunctions()));
 
         // foreach individual in offspring population, do
         for (T item : offspringPopulation) {
             // Insert offspring this.floor(p) in the approximative archive Aeg such that
             // only non-dominated solutions remain
             // (Insert only if floor(p) is non-nominating?)
+            // (Insert all, then keep only best front?)
+
+            // Check if floor(item) is better than the first front of Aeg
+            
 
             // Discard offspring p if it's dominated by any point
             // this.increment(a), where a is part of A
             // (Discard p if it's dominated by any increment(a) in A)
 
+            offspringPopulation.remove(item);
             // IS THIS CORRECT? (if not dominated, add to population)
             this.population.add(item);
         }
 
         // Add offsprings to Population, P <-P union O
-        this.population = union(this.population, offspringPopulation);
+        // this.population = union(this.population, offspringPopulation);
 
         // While abs(Population) > mu, do
         if (this.population.size() > Properties.POPULATION) {
             // Remove individual from Population that is of least importance to the
             // approximation
-            // this should be a ranking function, right?
+            // (this should be a ranking function, right?)
 
             // IS THIS CORRECT? (sort according to rank and crowding distance)
             this.population.sort(new RankAndCrowdingDistanceComparator<>(true));
@@ -116,6 +124,8 @@ public class AGEII<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
                 // IS THIS CORRECT? (remove individuals)
                 this.population.remove(i);
         }
+        // (Update interate counter)
+        this.currentIteration++;
     }
 
     /**
@@ -124,11 +134,43 @@ public class AGEII<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
      * @param input d-dimensional objective vector x, archive parameter eg
      * @return Corresponding vector v on the epsilon-grid
      */
-    private void floor(T input) {
+    protected void floor(T input) {
         // for i = 1 to d do v[i] <- x[i]/epsilon
         for (final FitnessFunction<T> ff : this.getFitnessFunctions()) {
-            input.setFitness(ff, (input.getFitness(ff)) / Properties.EPSILON);
+            input.setFitness(ff, (Math.floor(input.getFitness(ff) / Properties.EPSILON)));
         }
+    }
+
+    /**
+     * Inserts non-dominating floor(p) from input into Aeg
+     * @param input List of objectives
+     */
+    protected void floorAndInsert(List<T> input) {
+        // floor all individuals
+        for (T item : input) {
+            floor(item);
+            // insert only if individual is not already in list
+            if (!this.approxArchive.contains(item)) {
+                this.approxArchive.add(item);
+            }
+        }
+        // sort (preferably "FastNonDominatedSorting")
+        this.rankingFunction.computeRankingAssignment(this.approxArchive, new LinkedHashSet<FitnessFunction<T>>(this.getFitnessFunctions()));
+        
+        // retain only the non-dominating individuals
+        this.approxArchive.retainAll(this.rankingFunction.getSubfront(0));
+    }
+
+    protected List<T> floorPopulation(List<T> input) {
+        List<T> tmp = new ArrayList<>(input.size());
+        for (T indiv : input) {
+            T tmpIndiv = indiv.clone();
+            for (FitnessFunction<T> ff : this.getFitnessFunctions()) {
+                tmpIndiv.setFitness(ff, (Math.floor(tmpIndiv.getFitness(ff) / Properties.EPSILON)));
+            }
+            tmp.add(tmpIndiv);
+        }
+        return tmp;
     }
 
     /**
@@ -137,29 +179,50 @@ public class AGEII<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
      * @param input d-dimensional vector x, archive parameter eg
      * @return Corresponding vector v that has each of its components increased by 1
      */
-    private void increment(T input) {
+    protected void increment(T input) {
         // for i = 1 to d do v[i] <- o[i]+1
     }
 
     @Override
     public void generateSolution() {
-        // STUB
+        logger.info("executing generateSolution function");
+
+        if (population.isEmpty()) {
+            initializePopulation();
+        }
+
+        while (!isFinished()) {
+            evolve();
+            this.notifyIteration();
+            this.writeIndividuals(this.population);
+        }
+
+        notifySearchFinished();
     }
 
     @Override
     public void initializePopulation() {
+        // Logging and notifying
+        logger.info("executing initializePopulation function");
+        notifySearchStarted();
+        currentIteration = 0;
+
         // initialize population with random individuals
         this.generateInitialPopulation(Properties.POPULATION);
 
         // set grid resolution of the approximate archive
 
         // foreach individual in Population, do
-        for (T indiv : this.population) {
-            // Insert offspring floor(p) in the approximative archive Aeg such that only
-            // non-dominated solutions remain
-            // this.floor(this.population.get(i))
-            // Archive.getArchiveInstance()
-        }
+        List<T> tmpPop = new ArrayList<>(Properties.POPULATION);
+        tmpPop.addAll(this.population);
+        
+        // Insert offspring floor(p) in the approximative archive Aeg such that only
+        // non-dominated solutions remain
+        // TODO: Create test to see if duplicates are put in approxArchive
+        this.floorAndInsert(tmpPop);
+
+        // Notify
+        this.notifyIteration();
     }
 
     // Union function
--- master/src/test/java/org/evosuite/ga/metaheuristics/AGEIISystemTest.java
+++ master/src/test/java/org/evosuite/ga/metaheuristics/AGEIISystemTest.java
@@ -1,5 +1,139 @@
 package org.evosuite.ga.metaheuristics;
 
-public class AGEIISystemTest {
+import org.evosuite.SystemTestBase;
+import org.evosuite.testsuite.TestSuiteChromosome;
+import org.evosuite.coverage.branch.BranchCoverageSuiteFitness;
+import org.evosuite.coverage.line.LineCoverageSuiteFitness;
+import org.evosuite.ga.FitnessFunction;
+import org.evosuite.ga.operators.ranking.RankBasedPreferenceSorting;
+import org.evosuite.ga.operators.ranking.RankingFunction;
+import org.evosuite.Properties;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.Iterator;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anySet;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+public class AGEIISystemTest extends SystemTestBase {
+    @Test
+    public void testFloorMethod() {
+    // Set epsilon value
+    Properties.EPSILON = 0.1;
+
+    // Create fitness and chromosome
+    BranchCoverageSuiteFitness branch = new BranchCoverageSuiteFitness();
+    TestSuiteChromosome t1 = new TestSuiteChromosome();
     
+    // Set finess
+    t1.setFitness(branch, 0.5);
+    
+    // Initialize algorithm and set fitness function
+    AGEII<TestSuiteChromosome> algorithm = new AGEII<>(null);
+    algorithm.addFitnessFunction(branch);
+    
+    // Execute method
+    algorithm.floor(t1);
+
+    // Assert floored value
+    assertEquals(0.25, t1.getFitness(branch), 0.01);
+
+    }
+    
+    // Test for increment() method
+    @Test
+    public void testIncrementMethod() {
+        assertTrue(true);
+    }
+
+    // test for floorAndInsert() method
+    @Test
+    public void testFloorAndInsertMethod() {
+        Properties.EPSILON = 0.1;
+        // coverages
+        BranchCoverageSuiteFitness branch = new BranchCoverageSuiteFitness();
+        LineCoverageSuiteFitness line = new LineCoverageSuiteFitness();
+        
+        // algorithm
+        AGEII<TestSuiteChromosome> ga = new AGEII<>(null);
+        ga.addFitnessFunction(branch);
+        ga.addFitnessFunction(line);
+
+        // first chromosome
+        TestSuiteChromosome t1 = new TestSuiteChromosome();
+        t1.addFitness(branch, 0.5);
+        t1.addFitness(line, 0.3);
+
+        // clones of first chromosome (to use later)
+        TestSuiteChromosome t1c = t1.clone();
+
+        // add first chromosome to temporary list
+        List<TestSuiteChromosome> tmp = new ArrayList<TestSuiteChromosome>();
+        tmp.add(t1);
+
+        ga.floorAndInsert(tmp);
+
+        // assert that it got inserted
+        assertFalse(ga.approxArchive.isEmpty());
+
+        // create a temporary floored list
+        List<TestSuiteChromosome> floored_tmp = new ArrayList<TestSuiteChromosome>();
+        
+        // add floored chromosome to floored list
+        floored_tmp.add(t1);
+        
+        // assert that it was floored correctly
+        assertTrue(floored_tmp.containsAll(ga.approxArchive) && ga.approxArchive.containsAll(floored_tmp));
+
+       
+        // try flooring the previous chromosome again
+        tmp.clear();
+        tmp.add(t1c);
+        ga.floorAndInsert(tmp);
+        
+        // assert that duplicate was not inserted
+        assertTrue(floored_tmp.containsAll(ga.approxArchive) && ga.approxArchive.containsAll(floored_tmp));
+
+        // Create a new chromosome with better fitness
+        TestSuiteChromosome t2 = new TestSuiteChromosome();
+        t2.addFitness(branch, 0.4);
+        t2.addFitness(line, 0.2);
+        
+        // try to add it to the list 
+        tmp.clear();
+        tmp.add(t2);
+        ga.floorAndInsert(tmp);
+
+        floored_tmp.clear();
+        floored_tmp.add(t2);
+        
+        // assert that dominating was inserted, and previous was removed
+        assertTrue(floored_tmp.containsAll(ga.approxArchive) && ga.approxArchive.containsAll(floored_tmp));
+
+
+        // add a new chromosome with worse fitness
+        TestSuiteChromosome t3 = new TestSuiteChromosome();
+        t3.addFitness(branch, 1.0);
+        t3.addFitness(line, 1.0);
+
+
+        // try to add it to the list
+        tmp.clear();
+        tmp.add(t3);
+        ga.floorAndInsert(tmp);
+
+        // assert that worse individual was not inserted, an previous non-dominated remains
+        assertTrue(floored_tmp.containsAll(ga.approxArchive) && ga.approxArchive.containsAll(floored_tmp));
+    }
+
+    // test for update loop
 }
