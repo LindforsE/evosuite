8c7382836dad1be15f5d5e3de17e7fd5e269827f
LindforsE <45037228+LindforsE@users.noreply.github.com>
2022-06-27 09:04:29 +0200

    Updates to viewStatistics.py
    Initial commit to view CSV-files with more than one row.

--- viewStatistics.py
+++ viewStatistics.py
@@ -1,7 +1,7 @@
 import os
 # import numpy
-# from matplotlib import pyplot
-import pandas
+from matplotlib import pyplot
+from pandas import read_csv, Series, DataFrame, errors
 
 # go to correct directory
 os.chdir("bin/SF100")
@@ -13,19 +13,24 @@ with open("100chosenClasses.txt", 'r') as file1:
         dirFolders.add(item.split('\t')[0])
 
 # loop through each directory
+"""
+#frames = list()
 for item in dirFolders:
     os.chdir(f"{item}/")
-    # print(os.listdir())
+    print(os.listdir())
     try:
-        stat = pandas.read_csv("evosuite-report/statistics.csv", delimiter=',')
+        stat = read_csv(
+            filepath_or_buffer="evosuite-report/statistics.csv",
+            delimiter=',')
+        # frames.append(stat)
         # cat1 = stat['TARGET_CLASS']
         # print(stat.to_string())
-    except FileNotFoundError:
+    except (FileNotFoundError, errors.ParserError):
         pass
-    os.chdir("../")
+    os.chdir("../")"""
 
-os.chdir("104_vuze/evosuite-report/")
-stat2 = pandas.read_csv("statistics.csv", delimiter=',')
+os.chdir("101_netweaver/evosuite-report/")
+stat2 = read_csv("statistics.csv", delimiter=',')
 
 # timelines to be merged (_T1 -> _T60)
 #   CoverageTimeline
@@ -46,68 +51,83 @@ lineCoverageTimeline_list = [
     f"LineCoverageTimeline_T{num}" for num in range(1, 60)]
 branchCoverageTimeline_list = [
     f"BranchCoverageTimeline_T{num}" for num in range(1, 60)]
-exceptionCoverageTimeline_list = [
-    f"ExceptionCoverageTimeline_T{num}" for num in range(1, 60)]
-weakMutationCoverageTimeline_list = [
-    f"WeakMutationCoverageTimeline_T{num}" for num in range(1, 60)]
-outputCoverageTimeline_list = [
-    f"OutputCoverageTimeline_T{num}" for num in range(1, 60)]
+# exceptionCoverageTimeline_list = [
+#     f"ExceptionCoverageTimeline_T{num}" for num in range(1, 60)]
+# weakMutationCoverageTimeline_list = [
+#     f"WeakMutationCoverageTimeline_T{num}" for num in range(1, 60)]
+# outputCoverageTimeline_list = [
+#     f"OutputCoverageTimeline_T{num}" for num in range(1, 60)]
 methodCoverageTimeline_list = [
     f"MethodCoverageTimeline_T{num}" for num in range(1, 60)]
-methodNoExceptionCoverageTimeline_list = [
-    f"MethodNoExceptionCoverageTimeline_T{num}" for num in range(1, 60)]
-cbranchCoverageTimeline_list = [
-    f"CBranchCoverageTimeline_T{num}" for num in range(1, 60)]
+# methodNoExceptionCoverageTimeline_list = [
+#     f"MethodNoExceptionCoverageTimeline_T{num}" for num in range(1, 60)]
+# cbranchCoverageTimeline_list = [
+#     f"CBranchCoverageTimeline_T{num}" for num in range(1, 60)]
 fitnessTimeline_list = [
     f"FitnessTimeline_T{num}" for num in range(1, 60)]
 diversityTimeline_list = [
     f"DiversityTimeline_T{num}" for num in range(1, 60)]
 
+# for item in stat2["TARGET_CLASS"].values:
+#     print(item)
 
-coverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=coverageTimeline_list,
-    name="overallCoverage")
-lineCoverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=lineCoverageTimeline_list,
-    name="lineCoverate")
-branchCoverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=branchCoverageTimeline_list,
-    name="branchCoverage")
-exceptionCoverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=exceptionCoverageTimeline_list,
-    name="exceptionCoverage")
-weakMutationCoverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=weakMutationCoverageTimeline_list,
-    name="weakMutationCoverage")
-outputCoverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=outputCoverageTimeline_list,
-    name="outputCoverage")
-methodCoverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=methodCoverageTimeline_list,
-    name="methodCoverage")
-methodNoExceptionCoverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=methodNoExceptionCoverageTimeline_list,
-    name="methodNoExceptionCoverage")
-cbranchCoverageSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=cbranchCoverageTimeline_list,
-    name="cbranchCoverage")
-fitnessSeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=fitnessTimeline_list,
-    name="fitness")
-diversitySeries = pandas.Series(
-    data=stat2.squeeze(),
-    index=diversityTimeline_list,
-    name="diversity")
+cat = stat2[stat2['TARGET_CLASS'] == 'com.sap.engine.services.dc.wsgate.Stop']
+# print(cat)
+row = cat[coverageTimeline_list]
+row_mean = row.mean()
+# print(row)
+# print(row_mean)
+
+row.plot(kind="box")
+pyplot.show()
+# row.plot()
+# pyplot.show()
+# coverageSeries = pandas.Series(
+#    data=cat.squeeze(),
+#    name="overallCoverage")
+# coverageSeries.plot()
+# pyplot.show()
+
+# lineCoverageSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=lineCoverageTimeline_list,
+#     name="lineCoverate")
+# branchCoverageSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=branchCoverageTimeline_list,
+#     name="branchCoverage")
+# exceptionCoverageSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=exceptionCoverageTimeline_list,
+#     name="exceptionCoverage")
+# weakMutationCoverageSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=weakMutationCoverageTimeline_list,
+#     name="weakMutationCoverage")
+# outputCoverageSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=outputCoverageTimeline_list,
+#     name="outputCoverage")
+# methodCoverageSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=methodCoverageTimeline_list,
+#     name="methodCoverage")
+# methodNoExceptionCoverageSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=methodNoExceptionCoverageTimeline_list,
+#     name="methodNoExceptionCoverage")
+# cbranchCoverageSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=cbranchCoverageTimeline_list,
+#     name="cbranchCoverage")
+# fitnessSeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=fitnessTimeline_list,
+#     name="fitness")
+# diversitySeries = pandas.Series(
+#     data=stat2.squeeze(),
+#     index=diversityTimeline_list,
+#     name="diversity")
 
 # pickle all series'
 # pandas.to_pickle()
