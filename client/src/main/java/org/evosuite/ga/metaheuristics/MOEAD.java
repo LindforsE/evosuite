4fea0e3ff3aa65e5aa7795ff36cbc284633cac44
LindforsE <45037228+LindforsE@users.noreply.github.com>
2022-08-10 17:27:28 +0200

    Weight vectors for MOEAD.
    Implemented the Incremental Lattice Design for initial weight vector creation.

--- client/src/main/java/org/evosuite/ga/metaheuristics/MOEAD.java
+++ client/src/main/java/org/evosuite/ga/metaheuristics/MOEAD.java
@@ -1,14 +1,19 @@
 package org.evosuite.ga.metaheuristics;
 
+import org.evosuite.Properties;
 import org.evosuite.ga.Chromosome;
 import org.evosuite.ga.ChromosomeFactory;
+import org.evosuite.ga.FitnessFunction;
+import org.evosuite.runtime.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.*;
+
+
 /**
- * MOEA/D implementation
+ * MOEA/D implementation, Chebycheff approach, ILD weight vectors.
  * <INSERT ARTICLE HERE>
- * 
  * 
  * @author Elias
  */
@@ -16,13 +21,30 @@ public class MOEAD<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
     private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(MOEAD.class);
 
+    // external population (EP), stores non-dominated solutions found during the search
+    private final List<T> ep;
+
+    // weight vectors (points)
+    protected Double[][] weights;
+
+    // reference vector is origo, aka (0,0,0,...,0)
+    // does not need a variable
+
+    // z, best individual for each sub-problem
+    private final LinkedHashMap<FitnessFunction<T>, T> z;
+
+
     public MOEAD(ChromosomeFactory<T> factory) {
         super(factory);
+        ep = new ArrayList<>();
+        z = new LinkedHashMap<>();
+        weights = new Double[Properties.POPULATION][fitnessFunctions.size()];
     }
 
     @Override
     protected void evolve() {
-        // N = Number of sub (decomposed) problems
+        // pop = population
+        // N = Number of sub problems (objectives?)
         // for i= 1, ..., N, do
         for (int i = 0; i < 1; i++) {
 
@@ -45,6 +67,12 @@ public class MOEAD<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
 
             // step 2.2 Improvement
             // Apply a problem-specific repair/improvement heuristic on y to produce y'
+            if (Random.nextDouble() <= Properties.MUTATION_RATE) {
+                notifyMutation(offspring1);
+                offspring1.mutate();
+                notifyMutation(offspring2);
+                offspring2.mutate();
+            }
 
             // step 2.3 Update z
             // ...
@@ -82,6 +110,15 @@ public class MOEAD<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
         // INITIALIZATION
         // step 1.1
         // EP = empty set
+        // done
+
+        for (FitnessFunction<T> f : fitnessFunctions) {
+            z.put(f, null);
+        }
+
+        // distribute weight vectors (points)
+        this.weights = distributeWeightVectors();
+
 
         // step 1.2
         // Compute the Euclidean distances between any
@@ -91,8 +128,102 @@ public class MOEAD<T extends Chromosome<T>> extends GeneticAlgorithm<T> {
         // step 1.3
         // Generate an initial population
         // randomly or by a problem-specific method.
+        this.generateInitialPopulation(Properties.POPULATION);
 
         // step 1.4
         // Initialize by a problem-specific method.
+    }
+
+    /**
+     * Incremental lattice design (ILD), as specified in
+     * @inproceedings{10.1145/3377929.3398082,
+     * author = {Takagi, Tomoaki and Takadama, Keiki and Sato, Hiroyuki},
+     * title = {Incremental Lattice Design of Weight Vector Set},
+     * year = {2020},
+     * isbn = {9781450371278},
+     * publisher = {Association for Computing Machinery},
+     * address = {New York, NY, USA},
+     * url = {https://doi-org.miman.bib.bth.se/10.1145/3377929.3398082},
+     * doi = {10.1145/3377929.3398082},
+     * booktitle = {Proceedings of the 2020 Genetic and Evolutionary Computation Conference Companion},
+     * pages = {1486–1494},
+     * numpages = {9},
+     * keywords = {weight vector set, many-objective optimization, multi-objective optimization, uniform mixture design, evolutionary algorithm},
+     * location = {Canc\'{u}n, Mexico},
+     * series = {GECCO '20}
+     * }
+     */
+    protected Double[][] distributeWeightVectors() {
+        Set<Double[]> L = new TreeSet<>(MOEAD::compare);
+        List<Double[]> Lprim = new ArrayList<>();
+        List<Double[]> edge = new ArrayList<>();
+
+        // create origin point and add to L and edge
+        Double[] originPoint = new Double[fitnessFunctions.size()];
+        Arrays.fill(originPoint, 0.0);
+        edge.add(originPoint);
+        L.add(originPoint);
+
+        int H = 0;
+        // until we have one "vector" for each individual in population
+        while (L.size() < Properties.POPULATION) {
+            Lprim.clear();
+            Lprim.addAll(L);
+            L.clear();
+
+            // create new double from edge and increase specific "fitness" by fitness-size, and put into L
+            for (Double[] a : edge) {
+                for (int i = 0; i < fitnessFunctions.size(); i++) {
+                    // create new points for fitness'
+                    Double[] tmp = a.clone();
+                    tmp[i] += fitnessFunctions.size();
+                    L.add(tmp);
+                }
+            }
+            // get edges from L (all elements with a zero in it)
+            edge.clear();
+            int currentEdge = fitnessFunctions.size() -1;
+            for (Double[] a : L) {
+                for (Double aDouble : a) {
+                    if (aDouble == 0) {
+                        edge.add(a);
+                        break;
+                    }
+                }
+            }
+            // increase all L' by 1, and put into L
+            for (Double[] doubles : Lprim) {
+                for (int j = 0; j < fitnessFunctions.size(); j++) {
+                    doubles[j] += 1;
+                }
+                L.add(doubles);
+            }
+            H++;
+        }
+
+        // normalization
+        for (Double[] doubles : L) {
+            for (int j = 0; j < fitnessFunctions.size(); j++) {
+                doubles[j] = doubles[j] / (fitnessFunctions.size() * H);
+            }
+
+        }
+        return L.toArray(new Double[0][]);
+    }
+
+    private static int compare(Double[] doubles, Double[] t1) {
+        if (doubles.length == t1.length) {
+            for (int i = 0; i < doubles.length; i++) {
+                if (doubles[i] > t1[i])
+                    return 1;
+                else if (doubles[i] < t1[i])
+                    return -1;
+            }
+        } else {
+            if (doubles.length > t1.length)
+                return 1;
+            return -1;
+        }
+        return 0;
     }
 }
--- master/src/test/java/org/evosuite/ga/metaheuristics/MOEADSystemTest.java
+++ master/src/test/java/org/evosuite/ga/metaheuristics/MOEADSystemTest.java
@@ -1,9 +1,22 @@
 package org.evosuite.ga.metaheuristics;
 
+import org.evosuite.Properties;
 import org.evosuite.SystemTestBase;
+import org.evosuite.coverage.branch.BranchCoverageSuiteFitness;
+import org.evosuite.coverage.io.input.InputCoverageSuiteFitness;
+import org.evosuite.coverage.line.LineCoverageSuiteFitness;
+import org.evosuite.coverage.method.MethodCoverageSuiteFitness;
+import org.evosuite.coverage.statement.StatementCoverageSuiteFitness;
 import org.evosuite.ga.DummyChromosome;
+import org.evosuite.testsuite.TestSuiteChromosome;
 import org.junit.Test;
-import org.junit.Assert;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.*;
 
 public class MOEADSystemTest extends SystemTestBase {
 
@@ -11,13 +24,143 @@ public class MOEADSystemTest extends SystemTestBase {
     public void testEmptyPopulation() {
         MOEAD<DummyChromosome> ga = new MOEAD<>(null);
 
-        Assert.assertTrue(true);
+        assertTrue(ga.population.isEmpty());
     }
 
+    /*
     @Test
     public void testInitialPopulation() {
         MOEAD<DummyChromosome> ga = new MOEAD<>(null);
         ga.initializePopulation();
 
+    }
+    */
+
+    @Test
+    public void testDistributedWeight3x10() throws IOException {
+        // 10 by three dimensions (3 objectives)
+        Properties.POPULATION = 10;
+        LineCoverageSuiteFitness line = new LineCoverageSuiteFitness();
+        BranchCoverageSuiteFitness branch = new BranchCoverageSuiteFitness();
+        StatementCoverageSuiteFitness statement = new StatementCoverageSuiteFitness();
+
+        MOEAD<TestSuiteChromosome> ga = new MOEAD<>(null);
+        ga.addFitnessFunction(line);
+        ga.addFitnessFunction(branch);
+        ga.addFitnessFunction(statement);
+        Double[][] tmp = ga.distributeWeightVectors();
+
+        for (Double[] a : tmp) {
+         System.out.printf("%f, %f, %f%n", a[0], a[1], a[2]);
+        }
+
+        // assert # weights
+        assertEquals(Properties.POPULATION, tmp.length);
+
+        // assert weight values
+
+        double[][] front = readFront("ILD_M3_H2_N10.pf", 3);
+        assertEquals(front, tmp);
+
+    }
+
+    @Test
+    public void testDistributedWeight3x19() throws IOException {
+        // 19 by three dimensions (3 objectives)
+        Properties.POPULATION = 19;
+        LineCoverageSuiteFitness line = new LineCoverageSuiteFitness();
+        BranchCoverageSuiteFitness branch = new BranchCoverageSuiteFitness();
+        StatementCoverageSuiteFitness statement = new StatementCoverageSuiteFitness();
+
+        MOEAD<TestSuiteChromosome> ga = new MOEAD<>(null);
+        ga.addFitnessFunction(line);
+        ga.addFitnessFunction(branch);
+        ga.addFitnessFunction(statement);
+        Double[][] tmp = ga.distributeWeightVectors();
+
+        for (Double[] a : tmp) {
+            System.out.printf("%f, %f, %f%n", a[0], a[1], a[2]);
+        }
+
+        // assert # weights
+        assertEquals(Properties.POPULATION, tmp.length);
+
+        // assert weight values
+
+        double[][] front = readFront("ILD_M3_H3_N19.pf", 3);
+        assertEquals(front, tmp);
+
+    }
+
+    @Test
+    public void testDistributedWeight3x31() throws IOException {
+        // 31 by three dimensions (3 objectives)
+        Properties.POPULATION = 31;
+        LineCoverageSuiteFitness line = new LineCoverageSuiteFitness();
+        BranchCoverageSuiteFitness branch = new BranchCoverageSuiteFitness();
+        StatementCoverageSuiteFitness statement = new StatementCoverageSuiteFitness();
+
+        MOEAD<TestSuiteChromosome> ga = new MOEAD<>(null);
+        ga.addFitnessFunction(line);
+        ga.addFitnessFunction(branch);
+        ga.addFitnessFunction(statement);
+        Double[][] tmp = ga.distributeWeightVectors();
+
+        for (Double[] a : tmp) {
+            System.out.printf("%f, %f, %f%n", a[0], a[1], a[2]);
+        }
+
+        // assert # weights
+        assertEquals(Properties.POPULATION, tmp.length);
+
+        // assert weight values
+
+        double[][] front = readFront("ILD_M3_H4_N31.pf", 3);
+        assertEquals(front, tmp);
+
+    }
+
+    @Test
+    public void testDistributedWeight5x126() throws IOException {
+        // 126 by five dimensions (5 objectives)
+        Properties.POPULATION = 126;
+        LineCoverageSuiteFitness line = new LineCoverageSuiteFitness();
+        BranchCoverageSuiteFitness branch = new BranchCoverageSuiteFitness();
+        StatementCoverageSuiteFitness statement = new StatementCoverageSuiteFitness();
+        MethodCoverageSuiteFitness method = new MethodCoverageSuiteFitness();
+        InputCoverageSuiteFitness input = new InputCoverageSuiteFitness();
+
+        MOEAD<TestSuiteChromosome> ga = new MOEAD<>(null);
+        ga.addFitnessFunction(line);
+        ga.addFitnessFunction(branch);
+        ga.addFitnessFunction(statement);
+        ga.addFitnessFunction(method);
+        ga.addFitnessFunction(input);
+
+        Double[][] tmp = ga.distributeWeightVectors();
+
+        assertEquals(Properties.POPULATION, tmp.length);
+
+        double[][] front = readFront("ILD_M5_H4_N126.pf", 5);
+        assertEquals(front, tmp);
+    }
+    protected double[][] readFront(String name, int fronts) throws IOException{
+        double[][] front = new double[Properties.POPULATION][fronts];
+        int index = 0;
+
+        InputStream in = ClassLoader.getSystemResourceAsStream(name);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        String sCurrentLine;
+        while ((sCurrentLine = br.readLine()) != null) {
+            String[] split = sCurrentLine.split(",");
+            for (int i = 0; i < fronts; i++) {
+                front[index][i] = Double.parseDouble(split[i]);
+
+            }
+            index++;
+        }
+        br.close();
+        return front;
     }
 }
